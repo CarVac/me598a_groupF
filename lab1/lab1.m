2b32fae1449bf2c0aa8fa690d30285825812d4a4
CarVac <c.lo.to.da.down.lo@gmail.com>
2015-02-16 22:02:17 -0500

    "Change lab code to call new function."

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -11,16 +11,6 @@ theta3 = 0;
 theta4 = 0;
 theta5 = 0;
 
-T1 = DH_trans(0, pi/2, 67/25.4, theta1*pi/180);
-P1 = T1 * [0 0 0 1]'
-T2 = DH_trans(90/25.4, 0, 0, theta2*pi/180);
-P2 = T1 * T2 * [0 0 0 1]'
-T3 = DH_trans(78/25.4, 0, 0, theta3*pi/180);
-P3 = T1 * T2 * T3 * [0 0 0 1]'
-T4 = DH_trans(0, pi/2, 0, theta4*pi/180);
-P4 = T1 * T2 * T3 * T4 * [0 0 0 1]'
-T5 = DH_trans(0, 0, 142/25.4, theta5*pi/180);
-
-transform = T1*T2*T3*T4*T5;
+transform = robotTransform(theta1, theta2, theta3, theta4, theta5);
 
 position = transform*[0;0;0;1]
